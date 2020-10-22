27d55cfb510771dfcc8b2a5bc0b2674c88593fee
Dev <64605024+thundersparkf@users.noreply.github.com>
2020-10-23 00:03:18 +0530

    Update executor.py

--- text_summarizer/executor.py
+++ text_summarizer/executor.py
@@ -3,7 +3,7 @@ from core import Summary
 class Executor:
     def __init__(self, url):
         self.scrape = Scraper(url)
-        self.summary = self.Summary()
+        self.summary = Summary()
     def execute(self):
         data = self.scrape.scraper()
         text = self.summary.summarize(data['linkless'])
